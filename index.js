2cab8645d30f8a5012aa00aabb4178f4ae14900c
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2022-11-11 22:08:22 +0530

    Initial comit

---
+++ index.js
@@ -0,0 +1,20 @@
+// const express = require("express");
+// const bodyParser = require ("body-parser");
+// const request = require("request");
+
+// const app=express();
+
+// app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({extended: true}))
+
+// app.get("/",function(req,res){
+//     res.sendFile(__dirname+"/index.html");
+// })
+
+// app.post("/",function(req,res){
+//     res.sendFile(__dirname+"/success.html")
+// })
+
+// app.listen(process.env.PORT || 3000 , function(){
+//     console.log("Server running");
+// })
